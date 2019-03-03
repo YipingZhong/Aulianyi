8bf3f8e65dd36ecd4f590462ea7a934e09bcbdd9
LibanOdowa <liban.odowa@gmail.com>
2019-03-03 14:26:57 -0600

    Added project

---
+++ 2d Template/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    int time1 = 0;
+    Color c = Color.blue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        c = Color.Lerp(Color.blue, Color.green, time1/120);
+        gameObject.GetComponent<Renderer>().material.color = c;
+        print(time1);
+        if (time1 < 120)
+        {
+            transform.localScale += new Vector3(0.05F, 0.05F, 0);
+        }
+        if (time1 > 120 && time1 < 240)
+        {
+            transform.localScale += new Vector3(-0.05F, -0.05F, 0);
+        }
+        time1++;
+    }
+
+}
---
+++ 2d Template/Assets/boutscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boutscript : MonoBehaviour
+{
+    int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 120)
+        {
+            gameObject.SetActive(false);
+        }
+        if (time > 120 )
+        {
+            gameObject.SetActive(true);
+        }
+        time++;
+
+    }
+}
