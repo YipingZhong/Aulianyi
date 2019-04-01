949c43b685191b528d4314cd5b152c36f99780f0
LibanOdowa <liban.odowa@gmail.com>
2019-04-01 14:16:24 -0500

    Android Build & Touch
    - Added android build configuration
    
    - Still trying to make touch start animation

---
+++ Assets/TouchHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake() {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int nbTouches = Input.touchCount;
+ 
+        if(nbTouches > 0)
+        {
+            for (int i = 0; i < nbTouches; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+ 
+                if(touch.phase == TouchPhase.Ended) {
+                    print("released");
+                }
+            }
+        }
+        
+    }
+}
