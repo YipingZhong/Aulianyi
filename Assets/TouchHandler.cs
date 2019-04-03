a3d8f6a34da48ba8e1df9077cef9c8460203466c
LibanOdowa <liban.odowa@gmail.com>
2019-04-03 14:15:01 -0500

    Text changes
    - Cleaned up unused files
    - Tweaked color of sphere
    - Added center text (scaling is WIP)

--- Assets/TouchHandler.cs
+++ Assets/TouchHandler.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class TouchHandler : MonoBehaviour
 {
     public GameObject Sphere;
     public GameObject SphereOutline;
+    public GameObject BreatheText;
+
     Animator sphereanim;
     Animator outlineanim;
+    TMP_Text breatheText;
+
+    float cycleTime = 14f;
+    float counter = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +26,9 @@ public class TouchHandler : MonoBehaviour
     void Awake() {
         sphereanim = Sphere.GetComponent<Animator>();
         outlineanim = SphereOutline.GetComponent<Animator>();
+        breatheText = BreatheText.GetComponent<TMP_Text>();
+        breatheText.text = "Touch to start";
+        //breatheText.alignment = TextAnchor.MiddleCenter;
     }
 
     // Update is called once per frame
@@ -32,10 +43,32 @@ public class TouchHandler : MonoBehaviour
                 Touch touch = Input.GetTouch(i);
  
                 if(touch.phase == TouchPhase.Ended) {
+                    counter = 0;
+                    StartCoroutine("breathe");
                     sphereanim.Play("Looping",0,0f);
                     outlineanim.Play("OutlineLooping",0,0f);
                 }
             }
+        }
+        
+    }
+
+    IEnumerator breathe()
+    {
+        while (counter < cycleTime)
+        {
+            if (counter < cycleTime/2)
+            {
+                breatheText.text = "Breathe In";
+                breatheText.fontSize += .1f;
+            }
+            else if (counter <= cycleTime)
+            {
+                breatheText.text = "Breathe Out";
+                breatheText.fontSize -= .1f;
+            }
+            counter += Time.deltaTime;
+            yield return null;
         }
         
     }
