d999668f658655560bfe2cf7b764e606fffd7688
LibanOdowa <liban.odowa@gmail.com>
2019-05-06 17:46:45 -0500

    Added song and UI Fade
    - Song 2 added
    - Placeholder UI Text that fades

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -11,6 +11,7 @@ public class TouchHandler : MonoBehaviour
     public GameObject cam;
     public GameObject cameraEndPosition;
     public Image m_Image;
+    public Graphic t;
     public AudioSource song0;
     public AudioSource song1;
     public AudioSource song2;
@@ -34,6 +35,7 @@ public class TouchHandler : MonoBehaviour
         m_Image.CrossFadeAlpha(0, 2f, false);
         song0.Stop();
         song2.Stop();
+        StartCoroutine(fadeUI());
     }
 
     void Awake() {
@@ -43,7 +45,6 @@ public class TouchHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         //https://gist.github.com/cuibonobo/09709c38b218e1f3fd0d
         foreach (Touch touch in Input.touches) {
             if (touch.phase == TouchPhase.Began) {
@@ -61,8 +62,8 @@ public class TouchHandler : MonoBehaviour
                     if (xDistance > yDistance){
                         if (lp.x > fp.x) {   //Right swipe
                             if(currentScreen > 0) {
-                                StopCoroutine(fade());
-                                StartCoroutine(fade());
+                                StopCoroutine(fade(m_Image));
+                                StartCoroutine(fade(m_Image));
                                 if (currentScreen == 1){
                                     cameraEndPosition.transform.position = new Vector3(-100,0,-10);
                                     StartCoroutine(CrossFadeAudio(song1,song0,2f,1f));
@@ -76,8 +77,8 @@ public class TouchHandler : MonoBehaviour
                             }
                         } else {   //Left swipe
                             if(currentScreen < 2) {
-                                StopCoroutine(fade());
-                                StartCoroutine(fade());
+                                StopCoroutine(fade(m_Image));
+                                StartCoroutine(fade(m_Image));
                                 if (currentScreen == 0){
                                     cameraEndPosition.transform.position = new Vector3(0,0,-10);
                                     StartCoroutine(CrossFadeAudio(song0,song1,2f,1f));
@@ -94,6 +95,7 @@ public class TouchHandler : MonoBehaviour
                     } 
                 } else {
                     // That was a tap!
+
                 }
             }
             // Just do the one touch for now...
@@ -104,12 +106,16 @@ public class TouchHandler : MonoBehaviour
 
     }
 
-    IEnumerator fade() {
-        m_Image.CrossFadeAlpha(1, 0.2f, false);
+    IEnumerator fade(Image g) {
+        g.CrossFadeAlpha(1, 0.2f, false);
         yield return new WaitForSeconds(0.7f);
-        m_Image.CrossFadeAlpha(0, 2f, false);
+        g.CrossFadeAlpha(0, 2f, false);
     }
 
+    IEnumerator fadeUI() {
+        yield return new WaitForSeconds(3.0f);
+        t.CrossFadeAlpha(0,2.0f,false);
+    }
 
     private IEnumerator CrossFadeAudio(AudioSource audioSource1, AudioSource audioSource2, float crossFadeTime, float audioSource2VolumeTarget)
     {
