fcfc95ef3697ea14a85ed2ea4c801058d9f87779
Yiping Zhong <yzhong@macalester.edu>
2019-05-07 17:39:26 -0500

    Try to make realtime scrolling less sensitive

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -55,7 +55,8 @@ public class TouchHandler : MonoBehaviour
             if (touch.phase == TouchPhase.Moved) {
                 // move camera directly (no animation!)
                 // to originalCameraPosition + (touch.position.x - fp.x, 0, 0)
-                cameraEndPosition.transform.position = cameraEndPosition.transform.position + new Vector3(touch.position.x - fp.x,0,-10);
+                float x = (touch.position.x - fp.x)/10;
+                cameraEndPosition.transform.position = cameraEndPosition.transform.position + new Vector3(x, 0,-10);
             }
             if (touch.phase == TouchPhase.Ended) {
                 lp = touch.position;
